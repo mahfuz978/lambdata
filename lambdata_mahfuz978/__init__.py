e6867ad466d83702c62c01d498b5ed565ca1960a
mahfuz978 <mahfuz978@users.noreply.github.com>
2020-01-14 12:36:48 -0500

    added some proof of concept utility files

--- lambdata_mahfuz978/__init__.py
+++ lambdata_mahfuz978/__init__.py
@@ -1,5 +1,8 @@
 """
 lambdata - a collection of data science helper function
 """
-import pandas as import pd
+import pandas as pd
 import numpy as np
+
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
---
+++ lambdata_mahfuz978/ds_utils.py
@@ -0,0 +1,7 @@
+"""
+utility functions for working with Data
+"""
+
+import pandas
+
+TEST_DF = pandas.DataFrame([1,2,3])
