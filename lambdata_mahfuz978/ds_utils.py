0ce27985aeeec061e70ecc70ee461b7721630c3c
mahfuz978 <mahfuz978@users.noreply.github.com>
2020-01-14 19:12:52 -0500

    new_version added function

--- lambdata_mahfuz978/ds_utils.py
+++ lambdata_mahfuz978/ds_utils.py
@@ -5,3 +5,20 @@ utility functions for working with Data
 import pandas
 
 TEST_DF = pandas.DataFrame([1,2,3])
+
+
+d = {'imdb_score' : [1,1.4,1,2.4,2,3,4.8,5,6,6.9,6,6,7,7.5,7.9, 8,9,10,11,12,13,14]}
+movie = pandas.DataFrame(data=d)
+
+def bin_rating(rating):
+  if rating <= 5:
+    return 'poor'
+  elif rating <= 7:
+    return 'average'
+  elif rating <= 8:
+    return 'good'
+  else:
+    return 'excellent'
+
+movie['rating_bin'] = movie['imdb_score'].apply(bin_rating) 
+
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
     name="lambdata-mahfuz978",
-    version = "0.1.1",
+    version = "0.1.2",
     author = "mahfuz978",
     description = "a collection of data science helper functions",
     long_description = LONG_DESCRIPTION,
